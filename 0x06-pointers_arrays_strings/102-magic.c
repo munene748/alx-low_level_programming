88232908191b32f1fd183caf911ef2496f863aa5
munene748 <muneneemmanuel22@gmail.com>
2023-07-28 01:53:06 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    int a[] = {94, 55, 98, 67, 89};
-    int *p = a;
-  
-    // Add the new line of code below this line
-    printf("a[2] = %d\n", *(p + 2));
+int main(void) {
+    int n;
+    int a[5];
+    int *p;
 
-    printf("a = %p\n", a);
-    printf("p = %p\n", p);
-  
+    a[2] = 1024;
+    p = &n;
+    /*
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use a
+     * - you are not allowed to modify p
+     * - only one statement
+     * - you are not allowed to code anything else than this line of code
+     */
+    *(p + 7) = 98;
+    /* ...so that this prints 98\n */
+    printf("a[2] = %d\n", a[2]);
     return 0;
 }
 
