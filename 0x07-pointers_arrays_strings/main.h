5a57506d822aadb9d56772403d1c94aee1146a76
munene748 <muneneemmanuel22@gmail.com>
2023-08-01 00:12:18 +0300

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-MAIN.H code
 #ifndef MAIN_H
 #define MAIN_H
+
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -13,4 +13,3 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif
-
