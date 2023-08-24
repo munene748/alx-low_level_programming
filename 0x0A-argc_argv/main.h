89d80a09a107af9afb2e16ff87ccb6bccb946eca
munene748 <muneneemmanuel22@gmail.com>
2023-08-24 13:12:51 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes the character c to stdout.
+ * @c: The character to print.
+ *
+ * Return: On success, 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s);
+
+#endif /* MAIN_H */
+
