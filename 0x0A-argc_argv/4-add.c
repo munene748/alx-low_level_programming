1596ab08ec69af785673cfb0d7da46059ba81a08
munene748 <muneneemmanuel22@gmail.com>
2023-09-02 01:44:46 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,74 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
 
-/**
- * check_num - check if a string contains only digits
- * @str: input string
- *
- * Return: 1 if the string contains only digits, 0 otherwise
- */
-int check_num(char *str)
-{
-<<<<<<< HEAD
- unsigned int count = 0;
-
- while (count < strlen(str))
-  {
- if (!isdigit(str[count]))
-  {
- return (0);
-  }
- count++;
- }
- return (1);
-=======
-    unsigned int count;
-
-    count = 0;
-    while (count < strlen(str))
-    {
-        if (!isdigit(str[count]))
-        {
+int check_num(char *str) {
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (!isdigit(str[i])) {
             return 0;
         }
-        count++;
     }
     return 1;
->>>>>>> b61c7479b025a5063d332acf9d25edb3e4394bde
 }
 
-/**
- * main - Print the sum of valid integer arguments
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, 1 on error
- */
-int main(int argc, char *argv[])
-{
- int count;
- int str_to_int;
- int sum = 0;
-
-    count = 1;
-    while (count < argc)
-    {
-        if (check_num(argv[count]))
-        {
-            str_to_int = atoi(argv[count]);
-            sum += str_to_int;
-        }
-        else
-        {
-            printf("Error\n");
-            return 1;
-        }
-        count++;
+int main(int argc, char *argv[]) {
+    int i, sum = 0;
+    
+    if (argc < 2) {
+        printf("Usage: %s <num1> <num2> ... <numN>\n", argv[0]);
+        return 1;
     }
 
-    printf("%d\n", sum);
+    for (i = 1; i < argc; i++) {
+        if (check_num(argv[i])) {
+            int str_to_int = atoi(argv[i]);
+            sum += str_to_int;
+        } else {
+            printf("Error: Argument %d is not a valid integer\n", i);
+            return 1;
+        }
+    }
+
+    printf("Sum: %d\n", sum);
 
     return 0;
 }
