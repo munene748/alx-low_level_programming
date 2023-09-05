a0f71ee61c40190e80784ef21d31f16aaaa86acf
munene748 <muneneemmanuel22@gmail.com>
2023-09-05 04:45:47 +0300

    0x0B-malloc_free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,26 +1,27 @@
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
-* *argstostr - concatenates all the arguments of the program
-* @ac: number of arguments
-* @av: array of arguments
-*
-* Return: Pointer to the new string (Success), NULL (Error)
-*/
+ * argstostr - Concatenates all the arguments of the program.
+ * @ac: Number of arguments.
+ * @av: Array of arguments.
+ *
+ * Return: Pointer to the new string (Success), NULL (Error).
+ */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
+	int i, j, k = 0, len = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+	/* Calculate the total length of the concatenated string */
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
-		len++;
+		len++; /* for the newline character */
 	}
 
 	str = malloc(sizeof(char) * (len + 1));
@@ -28,8 +29,7 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 
-	k = 0;
-
+	/* Copy the arguments into the allocated memory */
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -40,7 +40,9 @@ char *argstostr(int ac, char **av)
 		str[k] = '\n';
 		k++;
 	}
-	
+
+	str[k] = '\0'; /* Null-terminate the concatenated string */
+
 	return (str);
 }
 
