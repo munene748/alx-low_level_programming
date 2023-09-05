84968f12b0d23dbc3fe3da093159351a9ea38622
munene748 <muneneemmanuel22@gmail.com>
2023-09-05 23:38:11 +0300

    0x09-static_libraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+
+# Compile all .c files in the current directory into object files
+gcc -c *.c
+
+# Create the static library liball.a from the object files
+ar rc liball.a *.o
+
+# Index the library for quicker access
 ranlib liball.a
+
+# Clean up by removing the object files
+rm -f *.o
+
+# Print a message indicating the library has been created
+echo "Static library liball.a has been created."
 
