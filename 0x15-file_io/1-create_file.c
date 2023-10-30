a84011c0c56696d6d949b04b8713d6efad0e368d
munene748 <muneneemmanuel22@gmail.com>
2023-10-30 23:04:58 +0300

    file input-output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
- *
- * Return: 1 if success. -1 if fails.
+ * create_file - Create a file with specified permissions and content.
+ * @filename: Name of the file to create.
+ * @text_content: The content to write to the file.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int numletters;
-	int rwr;
+    int fd, rwr, numletters;
 
-	if (!filename)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return (-1);
 
-	if (fd == -1)
-		return (-1);
+    if (text_content != NULL)
+    {
+        for (numletters = 0; text_content[numletters]; numletters++)
+            ;
 
-	if (!text_content)
-		text_content = "";
+        rwr = write(fd, text_content, numletters);
+        if (rwr == -1 || rwr != numletters)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	for (numletters = 0; text_content[numletters]; numletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+    close(fd);
+    return (1);
 }
+
